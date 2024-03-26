1b9e4773a82689a78376b4e9acd59bb58e156f7a
ruska- <ruska.beridze@gmail.com>
2024-03-26 14:19:45 +0400

    added simple loading state

--- client/src/NavLayout.jsx
+++ client/src/NavLayout.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
-export function NavLayout(){
-    return (
-        <>
-          <Navbar />
-          <Outlet />
-        </>
-      );
+export function NavLayout() {
+  const { state } = useNavigation();
+
+  return (
+    <>
+      <Navbar />
+      {state === "loading" ? <h1>Loading...</h1> : <Outlet />}
+    </>
+  );
 }
